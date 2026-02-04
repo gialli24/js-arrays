a9ad687a45210ac54cfecdd1e191ddd7048dce4e
gialli24 <a.gialli2409@gmail.com>
2026-02-04 13:07:50 +0100

    Add exercise 6 and update log exercise 5

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -30,10 +30,12 @@ console.log("Primo: " + firstTeacher, "\nArray Modificato: " + teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa");
-console.log(teachers[teachers.length - 1]);
+console.log(teachers);
 
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
