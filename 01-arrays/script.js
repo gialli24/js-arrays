0a9eefdd8cde4a4e4ca7eff49345ab1ef2007b40
gialli24 <a.gialli2409@gmail.com>
2026-02-04 13:12:50 +0100

    Add exercise 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -42,7 +42,12 @@ console.log(teachers);
 const lewisIndex = teachers.indexOf("Lewis");
 console.log(lewisIndex);
 
-
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+
+if (isTeachersEmpty == 0) {
+  console.log("L'array è vuoto");
+} else {
+  console.log("L'array contiene elementi");
+}
