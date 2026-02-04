dd75a4c48c3326b44e87fc43598497134f540837
gialli24 <a.gialli2409@gmail.com>
2026-02-04 13:27:06 +0100

    Add bonus 5

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -52,4 +52,14 @@ for (let i = 0; i < teachers.length; i++) {
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i];
+
+  if (i != teachers.length - 1) {
+    teachersString += ", ";
+  }
+}
+
+console.log(teachersString);
