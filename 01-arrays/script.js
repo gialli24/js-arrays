ccd4073e76ec0ff262a695063911a450b9880dab
gialli24 <a.gialli2409@gmail.com>
2026-02-04 13:00:59 +0100

    Adjust exercise 3 pop method

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -20,11 +20,10 @@ console.log(teachers[4]);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers[teachers.length - 1];
-teachers.pop(length - 1);
+teachers.pop();
 console.log("Ultimo: " + lastTeacher, "\nArray Modificato: " + teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
